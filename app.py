ba37d406b63855af92cf23aadfaffbf5dc16a643
Dev Verma <devv6687@gmail.com>
2025-07-14 15:49:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def index():
     return render_template("index.html", prediction=prediction, user_input=user_input)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
